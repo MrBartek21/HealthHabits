04c7c9f5bf68765d90e22934dcad57f771e80e66
MrBartek21 <bartek.pacyna@onet.pl>
2020-10-14 08:23:24 +0200

    Bot

--- janek.php
+++ janek.php
@@ -1,33 +1,24 @@
-<?php
-    session_start();
-	require_once("Resources/Config.php");
 
-    $result = mysqli_query($Connect, "SELECT * FROM products");
-	while($row=mysqli_fetch_array($result)){
-		$ID = $row['ID'];
-        $Type = $row['Test'];
-
-        echo $ID."  ";
-
-        if($Type==""){
-            echo "0";
-        }else{
-            $Type = explode(", ", $Type);
-            
-            print_r($Type);
-
-            $kcal = $Type[1];
-            $prot = str_replace(",",".",$Type[2],$i);
-            $carb = str_replace(",",".",$Type[3],$i);
-            $fats = str_replace(",",".",$Type[4],$i);
-
-            $Connect->query("UPDATE products SET Kcal='$kcal' WHERE ID='$ID'");
-            $Connect->query("UPDATE products SET Proteins='$prot' WHERE ID='$ID'");
-            $Connect->query("UPDATE products SET Carbohydrates='$carb' WHERE ID='$ID'");
-            $Connect->query("UPDATE products SET Fats='$fats' WHERE ID='$ID'");
-
-        }
-        
-        echo "<br>";
-	}
-?>
+<div id="bot"></div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                function GetDailyHabits(id){
+                    $.get('Resources/Ajax/Bot.php',{id: id}, function(data){
+                        $("#bot").html(data);
+                    });
+                }
+				
+                id=1;
+				GetDailyHabits(id);
+                setInterval(function(){
+                    if(id>13579){
+                        
+                    }else{
+                        id+=1;
+					GetDailyHabits(id);
+                    }
+                    
+                },1000);
+            });
+        </script>
