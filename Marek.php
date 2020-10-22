ba9429eec17816a5f8ce6ee85545a633e74e5edc
MrBartek21 <bartek.pacyna@onet.pl>
2020-10-22 13:15:34 +0200

    Update Marek.php

--- Marek.php
+++ Marek.php
@@ -1,46 +1,13 @@
 <?php
-    require_once("Resources/Config.php");
-    function CPM($PPM, $Kactivity){
-        $CPM = $PPM + $Kactivity;
-        $CPM = round($CPM, 2);
-        return $CPM;
+    $tab = ['a','b','c','d','e','f','g'];
+    $x=97;
+    for($i=0; $i<count($tab); $i++){
+        $tasocName["$tab[$i]"] = $x;
+        $x++;
     }
-    function PAL($PPM, $CPM){
-        $PAL = $CPM / $PPM;
-        $PAL = round($PAL, 2);
-        return $PAL;   
+    while( list($klucz, $wartosc) = each( $tasocName) ){
+        echo "$klucz => $wartosc<BR>";
     }
-    function activity_kcal($Connect, $Aid, $Time, $UserID){
-        $result = mysqli_query($Connect, "SELECT * FROM persona WHERE UserID='$UserID'");
-		$row = $result->fetch_assoc();
-		$Weight = $row['Weight'];
-		$NOW = date("Y-m-d");
-		$Kactivity = mysqli_query($Connect, "SELECT SUM(KCAL) FROM useractivity WHERE UserID='$UserID' AND Date LIKE '$NOW' ");
-		print_r($Kactivity);
-		/*$PPM = $row['PPM'];		
-        $result = mysqli_query($Connect, "SELECT * FROM activity_met WHERE ID='$Aid'");
-		$row = $result->fetch_assoc();
-		$MET = $row['MET'];
-		
-        $kcal = ($MET/60)*$Weight*$Time;
-        $pkt = $kcal *0.1/2;
-        $Kactivity += $kcal;
-        $CPM = CPM($PPM, $Kactivity);
-        $PAL = PAL($PPM, $CPM);
-        $Connect->query("UPDATE persona SET SumKCAL='$Kactivity', Score=Score+'$pkt', CPM='$CPM',PAL='$PAL' Where UserID='$UserID'");
-        $Connect->query("INSERT INTO user_activity VALUES (NULL, '$UserID', '$Aid', '$Time', '$kcal', now() )");*/
-    }
-    
-    $weight=75;
-    $wzrost = 179;
-    $Aid = 12;
-    $Time = 60;
-    $Sex='M';
-    $UserID=3;  
-    
-    activity_kcal($Connect, $Aid, $Time, $UserID);
-    
-    
     
     
     
