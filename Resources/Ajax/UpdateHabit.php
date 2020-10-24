cc78865384bba38db9918f670309c208d10431ff
MrBartek21 <bartek.pacyna@onet.pl>
2020-10-24 22:37:05 +0200

    Update UpdateHabit.php

--- Resources/Ajax/UpdateHabit.php
+++ Resources/Ajax/UpdateHabit.php
@@ -11,5 +11,6 @@
 
     $Connect->query("INSERT INTO historyhabits VALUES (NULL, '$UserID', '$HabitsID', now(), '$Series')");
     $Connect->query("UPDATE persona SET Score=Score+5 WHERE UserID='$UserID'");
+    PlantLevel($Connect, $UserID);
     echo "OK";
 ?>
